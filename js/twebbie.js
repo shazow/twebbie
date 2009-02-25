b9e21d128efc2179ffdf1bf7861c8ff6966fd893
Andrey Petrov <shazow@gmail.com>
2009-02-25 03:38:17 -0500

    Basic fetching works. Adding drag/drop UI for member transplanting.

--- js/twebbie.js
+++ js/twebbie.js
@@ -7,39 +7,88 @@
 function TwitterGroup(name, target) {
     this.name = name;
     this.target = target;
-    this.members = false
+    this.whitelist = {};
+    this.blacklist = {};
     this.slots_left = 100;
 }
 
 TwitterGroup.prototype.is_member = function(member_id) {
-    return this.members[member_id];
+    return this.whitelist[member_id];
 }
 
-TwitterGroup.prototype.add_member = function(member_id) {
-    return this.members[member_id] = true;
+TwitterGroup.prototype.add_member = function(member_id, tweets) {
+    /// TODO: Convert tweets into list items and insert them into the appropriate positions in the timeline for this group.
+    this.whitelist[member_id] = true;
 }
 
 TwitterGroup.prototype.remove_member = function(member_id) {
-    return this.members[member_id] = false;
+    /// TODO: Remove all items from this.target which are owned by member_id and return their data in a list.
+    this.whitelist[member_id] = false;
+    // return []
 }
 
 TwitterGroup.prototype.add_tweet = function(tweet) {
-    $("<li></li>").html($()).prependTo(this.target);
+    var tweet_obj = $('<li><img class="profile_image" src="' + tweet.user.profile_image_url + '" alt="' + tweet.user.name + '" /><span class="time" title="' + tweet.created_at + '">' + relative_time(tweet.created_at) + '</span> <a class="user" href="javascript:addAddress(\'' + tweet.user.screen_name + '\')">' + tweet.user.screen_name + '</a><div class="tweet_text">' + tweet.text.replace(/(\w+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+)/g, '<a href="$1">$1</a>').replace(/[\@]+([A-Za-z0-9-_]+)/g, '<a href="http://twitter.com/$1">@$1</a>').replace(/[&lt;]+[3]/g, "<tt class='heart'>&#x2665;</tt>") + '</div></li>').draggable();
+    tweet_obj[0].member_id = tweet.user.id;
+    tweet_obj[0].tweet_id = tweet.id;
+    tweet_obj[0].group = this;
+    tweet_obj[0].data = tweet;
+
+    $(this.target).prepend(tweet_obj);
     this.slots_left--;
 
-    if(this.slots_left>0) return;
+    if(this.slots_left>=0) return;
+
     $("li:last", this.target).remove();
+    this.slots_left++;
 }
 
 TwitterGroup.prototype.add_tweet_maybe = function(tweet) {
     var member_id = tweet.user.id;
-    if(this.is_member(member_id)) this.add_tweet(tweet);
+    if(this.member_id === {} || this.is_member(member_id)) this.add_tweet(tweet);
 }
 
 /* Abstraction for a user's Twitter account. */
-function Twitter() {
-    var base_group = TwitterGroup("Everything Else");
-    this.groups = new Array(base_group);
+function Twitter(base_target) {
+    this.groups = new Array();
+    this.last_update = false;
+    this.last_id = false;
 }
 
-Twitter.prototype.
+Twitter.prototype.register_group = function(name, target) {
+    var group = new TwitterGroup(name, target);
+    this.groups.push(group);
+
+    /* Allow dragging between groups to add/remove members. */
+    $(target).droppable({
+        drop:   function(event, ui) { 
+                    var tweet = ui.draggable[0];
+                    console.log("Added member " + tweet.member_id + " to group " + group.name); 
+                    var member_tweets = tweet.group.remove_member(tweet.member_id);
+                    group.add_member(tweet.member_id, member_tweets);
+                }
+    });
+}
+
+/* Inspired by http://github.com/peterk/twoot */
+Twitter.prototype.refresh = function() {
+    var target_url = "http://twitter.com/statuses/friends_timeline.json?callback=?";
+    if(this.last_update) target_url += "&since=" + this.last_update;
+
+    var self = this;
+
+    console.log("Fetching: " + target_url);
+    $.getJSON(target_url, function(data) {
+        /// TODO: Optimize?
+        $.each(data.reverse(), function(i, tweet) {
+            if(self.last_id && tweet.id <= self.last_id) return; // Skip id's which we already have.
+            $.each(self.groups, function(j, group) {
+                // Attempt to add the tweet to each of the groups.
+                group.add_tweet_maybe(tweet);
+                self.last_id = tweet.id;
+            });
+        });
+    });
+
+    this.last_update = new Date().toGMTString();
+}
--- js/utils.js
+++ js/utils.js
@@ -9,3 +9,26 @@ search = function(o, v, i){
         else h = m;
     return o[h] != v ? i ? h : -1 : h;
 };
+
+
+/* Borrowed from http://github.com/seaofclouds/tweet/ */
+function relative_time(time_value) {
+    var parsed_date = Date.parse(time_value);
+    var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
+    var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
+    if(delta < 60) {
+    return 'less than a minute ago';
+    } else if(delta < 120) {
+    return 'about a minute ago';
+    } else if(delta < (45*60)) {
+    return (parseInt(delta / 60)).toString() + ' minutes ago';
+    } else if(delta < (90*60)) {
+    return 'about an hour ago';
+    } else if(delta < (24*60*60)) {
+    return 'about ' + (parseInt(delta / 3600)).toString() + ' hours ago';
+    } else if(delta < (48*60*60)) {
+    return '1 day ago';
+    } else {
+    return (parseInt(delta / 86400)).toString() + ' days ago';
+    }
+}
